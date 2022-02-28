29b1459934e96cf18b452b22cd92d31210f811b7
abyss-s <lh3729@naver.com>
2022-02-28 22:19:03 +0900

    prototype-inheritance

--- prototype-inheritance.js
+++ prototype-inheritance.js
@@ -1,3 +1,15 @@
 var superObj = {superVal: 'super'};
 var subObj = {subVal: 'sub'};
 
+subObj.__proto__ = superObj;
+
+console.log('subObj.subVal =>', subObj.subVal);
+console.log('subObj.superVal =>', subObj.superVal);
+
+//.__proto__ : subObj의 부모를 superObj로!
+//             찾았는데 없다면, 부모에서 가져다 씀.
+
+subObj.superVal = 'sub';
+console.log('superObj.superVal =>', superObj.superVal);
+
+// 자식 객체값을 바꿔도, 프로토 값은 바뀌지 않음. (얕은 복사같은 느낌!)
