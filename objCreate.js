2e8f3d58137fc47c6a066dc1a91dab5d0706d07f
abyss-s <lh3729@naver.com>
2022-03-03 23:54:01 +0900

    objCreate

--- objCreate.js
+++ objCreate.js
@@ -5,7 +5,7 @@ var superObj = {superVal: 'super'};
 
 var subObj = Object.create(superObj);
 
-// .__proto__와 같이 동작! 프로토타입 링크 지정.
+// .__proto__와 같이 동작. 프로토타입 링크 지정!
 // 괄호 안의 객체를 부모 객체로 취급!
 
 subObj.subVal = 'sub';
